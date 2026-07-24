6658b0acaf1833312e63629f670737ca323d7237
Emrektt <emreeken486@gmail.com>
2026-07-24 17:43:22 +0300

    Remove node_modules and old python backend to make TS primary language

--- backend_python_old/app/api/v1/announcements.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/announcements", tags=["Duyurular"])
-
-@router.get("/")
-def get_announcements():
-    return [{"id": 1, "title": "Hoş Geldiniz", "content": "Sisteme hoş geldiniz."}]
